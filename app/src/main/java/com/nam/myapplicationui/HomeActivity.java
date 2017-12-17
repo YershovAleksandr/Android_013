a80761e97a4310a4c3f52c4ba031af28dea35cd8
hp <namstudionsk@gmail.com>
2017-12-17 20:19:43 +0700

    Add Text & button

--- app/src/main/java/com/nam/myapplicationui/HomeActivity.java
+++ app/src/main/java/com/nam/myapplicationui/HomeActivity.java
@@ -4,15 +4,41 @@ import android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
     public static final String LOGTAG = "XLOG42";
 
+    public TextView mTextView;
+    public EditText mEditText;
+    public Button mButton;
+
+    private String StrTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        mTextView = findViewById(R.id.textView);
+        mEditText = findViewById(R.id.editText);
+        mButton = findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(LOGTAG, "Button click Motherfacka!!!");
+
+                StrTxt = mEditText.getText().toString();
+
+                Log.i(LOGTAG, "Edit Text = " + StrTxt);
+            }
+        });
 
         int i;
         float f;
@@ -37,6 +63,11 @@ public class HomeActivity extends AppCompatActivity {
         Log.i(LOGTAG, "XInt = " + i);
         Log.i(LOGTAG, "XFloat = " + f);
         Log.i(LOGTAG, "XString = " + s);
+
+
+        Log.i(LOGTAG, "TextView = " + mTextView.getText());
+        Log.i(LOGTAG, "EditText = " + mEditText.getText());
+        Log.i(LOGTAG, "Button = " + mButton.getText());
 
     }
 }
