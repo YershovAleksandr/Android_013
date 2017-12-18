0258f84a9bf401c2a0b2d983eecba6a0f57b38e8
hp <namstudionsk@gmail.com>
2017-12-19 02:35:47 +0700

    Add some code

--- app/src/main/java/com/nam/myapplicationui/NotificationActivity.java
+++ app/src/main/java/com/nam/myapplicationui/NotificationActivity.java
@@ -3,10 +3,18 @@ package com.nam.myapplicationui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class NotificationActivity extends AppCompatActivity {
 
     public static final String LOGTAG = "XLOG42";
+
+    private Button mButton2;
+    private Button mButton3;
+    private Button mButton4;
+    private Button mButton5;
+    private Button mButton6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,48 @@ public class NotificationActivity extends AppCompatActivity {
         Log.i(LOGTAG, "XInt = " + i);
         Log.i(LOGTAG, "XFloat = " + f);
         Log.i(LOGTAG, "XString = " + s);
+
+        mButton2 = findViewById(R.id.button2);
+        mButton3 = findViewById(R.id.button3);
+        mButton4 = findViewById(R.id.button4);
+        mButton5 = findViewById(R.id.button5);
+        mButton6 = findViewById(R.id.button6);
+
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(LOGTAG, "Button " + mButton2.getText().toString() + " Click!!");
+            }
+        });
+
+        mButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(LOGTAG, "Button " + mButton3.getText().toString() + " Click!!");
+            }
+        });
+
+        mButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(LOGTAG, "Button " + mButton4.getText().toString() + " Click!!");
+            }
+        });
+
+        mButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(LOGTAG, "Button " + mButton5.getText().toString() + " Click!!");
+            }
+        });
+
+        mButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(LOGTAG, "Button " + mButton6.getText().toString() + " Click!!");
+            }
+        });
+
 
 
     }
