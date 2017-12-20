8cb239ccbb09d5a37420c8c81b7e9e43ae99810f
hp <namstudionsk@gmail.com>
2017-12-20 21:22:02 +0700

    Fix lv

--- app/src/main/java/com/nam/myapplicationui/Main2Activity.java
+++ app/src/main/java/com/nam/myapplicationui/Main2Activity.java
@@ -1,8 +1,10 @@
 package com.nam.myapplicationui;
 
+import android.content.Context;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -13,9 +15,21 @@ public class Main2Activity extends AppCompatActivity {
 
 
         //getBaseContext().
+    }
 
-        //Snackbar.make(/*view*/ findViewById(R.layout.activity_main2), "Replace with your own action", Snackbar.LENGTH_LONG)
-          //      .setAction("Action", null).show();
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        View view = findViewById(R.id.clayout);
+
+
+
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+              .setAction("Action", null).show();
+
+
+
 
     }
 }
