eb8a9db28a8c4ebaefd30abc91e58fa739f8ff66
hp <namstudionsk@gmail.com>
2017-12-21 00:24:04 +0700

    Dev

---
+++ app/src/main/java/com/nam/myapplicationui/FrgmActivity.java
@@ -0,0 +1,19 @@
+/**
+ * Created by hp on 21.12.17.
+ */
+
+package com.nam.myapplicationui;
+
+import android.util.Log;
+
+public class FrgmStuff
+{
+    public static final String LOGTAG = "XLOG42";
+
+    public void FrgmStuff()
+    {
+        Log.i(LOGTAG, this.getClass().getName() + "Constructor");
+
+    }
+
+}
