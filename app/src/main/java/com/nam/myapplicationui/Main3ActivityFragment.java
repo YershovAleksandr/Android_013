b4ddb9408be4694fe6db7d1c53806f2227eb63c6
hp <namstudionsk@gmail.com>
2017-12-21 00:56:55 +0700

    Dev

--- app/src/main/java/com/nam/myapplicationui/Main3ActivityFragment.java
+++ app/src/main/java/com/nam/myapplicationui/Main3ActivityFragment.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.nam.myapplicationui.R;
 
 /**
  * A placeholder fragment containing a simple view.
