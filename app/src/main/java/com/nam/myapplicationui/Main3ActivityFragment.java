11ba726f6dbfcde2db22da9bbf0dd1c7ff97f90d
hp <namstudionsk@gmail.com>
2017-12-23 20:22:19 +0700

    Develop

--- app/src/main/java/com/nam/myapplicationui/Main3ActivityFragment.java
+++ app/src/main/java/com/nam/myapplicationui/Main3ActivityFragment.java
@@ -18,6 +18,12 @@ public class Main3ActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main3, container, false);
+
+        View infl;
+
+        infl = inflater.inflate(R.layout.activity_home, container, false);
+        //return inflater.inflate(R.layout.fragment_main3, container, false);
+
+        return infl;
     }
 }
